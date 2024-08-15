297cd3e1133679905decfe244f5a49667ef3489d
sasineelakandan <devasasi887@gmail.com>
2024-08-15 12:37:32 +0530

    finish all ui pages

---
+++ Msg/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import LoginComponent from './compenets/Login'
+import SignupComponent from './compenets/Siginup'
+import ProfilePage from './Profile'
+
+const App = () => {
+  return (
+    <div>
+      {/* <LoginComponent/> */}
+      {/* <SignupComponent/> */}
+      <ProfilePage/>
+    </div>
+  )
+}
+
+export default App
